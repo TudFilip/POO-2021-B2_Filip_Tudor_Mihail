499793afe7dbd04dad03ba61ea115ad18218a832
TudFilip <filiptudormihail@yahoo.com>
2021-02-19 00:49:17 +0200

    Exercitiul 2

--- exercitiul2 - lab1/main.cpp
+++ exercitiul2 - lab1/main.cpp
@@ -1,21 +1,41 @@
 #include <stdio.h>
+#include <iostream>
 
 using namespace std;
+
+int in_numar (char ch)
+{
+    return ch - '0';
+}
 
 int main()
 {
 	FILE *fis;
 	char ch;
 
-	fis = fopen("in.txt", "w+");
+    int numar = 0, cifra;
+    long suma = 0;
+	fis = fopen("in.txt", "r");
 
-	while (1)
+	while (!feof(fis))
 	{
-		ch = getc(fis);
-		printf("%c", ch);
+
+        ch = getc(fis);
+
+		if (ch >= 48 and ch <= 58)
+        {
+            cifra = in_numar(ch);
+            numar = numar*10 + cifra;
+        }
+        else
+        {
+            suma = suma + numar;
+            numar = 0;
+        }
+
 	}
 
+    printf("%d", suma);
 	fclose(fis);
-
 	return 0;
 }
