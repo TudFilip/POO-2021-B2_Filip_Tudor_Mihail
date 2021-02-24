46a53c6361cb3cc180cec07617acb82d1b821b45
TudFilip <filiptudormihail@yahoo.com>
2021-02-24 16:47:36 +0200

    "main modificat"

--- Lab2, ex1/main.cpp
+++ Lab2, ex1/main.cpp
@@ -2,6 +2,19 @@
 
 int main()
 {
+	NumberList lista1, lista2;
+
+	lista1.Init();
+	lista2.Init();
+
+	int x1, y1, z1;
+	cin >> x1 >> y1 >> z1;
+
+	lista1.Add(x1);
+	lista1.Add(y1);
+	lista1.Add(z1);
+
+
 
 	return 0;
 }
