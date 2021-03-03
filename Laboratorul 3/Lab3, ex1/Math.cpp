3bc10767faca2c0ea4e9c38dd149ea338154e114
TudFilip <filiptudormihail@yahoo.com>
2021-03-03 16:21:55 +0200

    putin din math.cpp si am facut fisierele pentru 2

---
+++ Laboratorul 3/Lab3, ex1/Math.cpp
@@ -0,0 +1,41 @@
+#include "Math.h"
+
+int Math::Add(int a, int b)
+{
+	return a + b;
+}
+
+int Math::Add(int a, int b, int c)
+{
+	return a + b + c;
+}
+
+int Math::Add(double a, double b)
+{
+	return a + b;
+}
+
+int Math::Add(double a, double b, double c)
+{
+	return a + b + c;
+}
+
+int Math::Mul(int a, int b)
+{
+	return a * b;
+}
+
+int Math::Mul(int a, int b, int c)
+{
+	return a * b * c;
+}
+
+int Math::Mul(double a, double b)
+{
+	return a * b;
+}
+
+int Math::Mul(double a, double b, double c)
+{
+	return a * b * c;
+}
