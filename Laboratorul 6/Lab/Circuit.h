7b445f9795e6be562453b42f47d52f27934bb56a
TudFilip <filiptudormihail@yahoo.com>
2021-03-24 16:22:18 +0200

    am adaugat main ul, de pe site, si clasa circuit

--- Laboratorul 6/Lab/Circuit.h
+++ Laboratorul 6/Lab/Circuit.h
@@ -1,1 +1,27 @@
 #pragma once
+#include <iostream>
+#include "Car.h"
+#include "Dacia.h"
+#include "Ford.h"
+#include "Mazda.h"
+#include "Mercedes.h"
+#include "Toyota.h"
+
+using namespace std;
+
+class Circuit
+{
+private:
+	double length;
+	int participants = 0;
+	Car* cars[100];
+	weather weatherType;
+
+public:
+	void SetLength(double value);
+	void SetWeather(weather givenWeatherType);
+	void AddCar(Car* car);
+	void Race();
+	void ShowFinalRanks();
+	void ShowWhoDidNotFinish();
+};
