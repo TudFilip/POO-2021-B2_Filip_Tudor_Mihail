1ef112853717f144712fa2a4b1216a8d4e476af8
TudFilip <filiptudormihail@yahoo.com>
2021-04-21 16:45:03 +0300

    terminat metodele get, set, clear, count plus iteratorul

--- Laboratorul 9/Lab9/Map_Template.h
+++ Laboratorul 9/Lab9/Map_Template.h
@@ -18,14 +18,46 @@ public:
 	
 	Map(): global_index(0) {}
 
-	Map begin()
+	class Iterator
 	{
-		return &objects[0];
-	}
+	public:
+		obj* p;
+		int it_index = 0;
+		Iterator& operator++()
+		{
+			p++;
+			it_index++;
+			return *this;
+		}
 
-	Map end()
+		bool operator !=(Iterator& i)
+		{
+			return p != i.p;
+		}
+
+		obj operator*()
+		{
+			obj p2;
+			p2.key = p->key;
+			p2.value = p->value;
+			p2.index = it_index;
+			return p2;
+		}
+	};
+
+	Iterator begin()
 	{
-		return &objects[global_index];
+		Iterator tmp;
+		tmp.it_index = 0;
+		tmp.p = &objects[0];
+		return tmp;
+	}
+	Iterator end()
+	{
+		Iterator tmp;
+		tmp.it_index = global_index;
+		tmp.p = &objects[global_index];
+		return tmp;
 	}
 
 	T2& operator [](const T1& key)
@@ -39,4 +71,50 @@ public:
 		objects[global_index].key = key;
 		return objects[global_index++].value;
 	}
+
+	void Set(const T1& key, const T2& value)
+	{
+		int i, ok = 0;
+		for (i = 0; i < global_index; i++)
+		{
+			if (objects[i].key == key)
+			{
+				ok = 1;
+				objects[i].value = value;
+				break;
+			}
+		}
+		if(!ok)
+			cout << "Nu exista cheia aleasa!\n";
+	}
+
+	bool Get(const T1& key, T2& value)
+	{
+		for (int i = 0; i < global_index; i++)
+		{
+			if (objects[i].key == key)
+			{
+				value = objects[i].value;
+				return true;
+			}
+		}
+		return false;
+	}
+
+	int Count()
+	{
+		return this->global_index;
+	}
+
+	void Clear()
+	{
+		for (int i = 0; i < global_index; i++)
+		{
+			objects[i].key = NULL;
+			objects[i].value = NULL;
+			objects[i].index = 0;
+		}
+		global_index = 0;
+	}
+
 };
